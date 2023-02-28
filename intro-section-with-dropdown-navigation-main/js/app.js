097266216a4108b10fc12bf97a36b81782d85992
Troneci Andrei <troneciandrei@gmail.com>
2023-02-28 19:02:49 +0200

    first commit

---
+++ intro-section-with-dropdown-navigation-main/js/app.js
@@ -0,0 +1,33 @@
+const btnOpen = document.querySelector("#btn-open");
+const btnClose = document.querySelector("#btn-close");
+const nav = document.querySelector("#nav");
+const body = document.body;
+const dropdownMenu = document.querySelectorAll(".dropdown-menu");
+const navBar = document.querySelector(".navbar");
+
+btnOpen.addEventListener("click", () => {
+  nav.classList.remove("hidden");
+  body.style.backgroundColor = "rgba(0,0,0,0.5)";
+});
+
+btnClose.addEventListener("click", () => {
+  nav.classList.add("hidden");
+  body.style.backgroundColor = "";
+});
+
+navBar.addEventListener("click", (event) => {
+  const target = event.target.closest(".nav-link");
+  if (!target) return;
+
+  const child = target.nextElementSibling;
+
+  dropdownMenu.forEach((menu) => {
+    if (menu !== child && !menu.classList.contains("hidden")) {
+      menu.classList.add("hidden");
+    }
+
+    if (menu === child && child.classList.contains("hidden")) {
+    }
+  });
+  child.classList.toggle("hidden");
+});
